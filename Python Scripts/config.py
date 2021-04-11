045efbb98e815bb9211293fcaf9a1d158e87a0b3
xerokami <jamiemc-100@hotmail.com>
2021-04-11 22:36:47 +0000

    Update config.py

--- Python Scripts/config.py
+++ Python Scripts/config.py
@@ -17,8 +17,6 @@ mdt_elk = "encoding encode-kvgpb\nstream yang-push\nupdate-policy periodic 1000\
 yang_xpath = ["filter xpath /memory-ios-xe-oper:memory-statistics/memory-statistic\n", 
               "filter xpath /process-cpu-ios-xe-oper:cpu-usage/cpu-utilization\n",
               "filter xpath /interfaces-ios-xe-oper:interfaces/interface/statistics\n",
-              "filter xpath /cdp-ios-xe-oper:cdp-neighbor-details\n",
               "filter xpath /ospf-ios-xe-oper:ospf-oper-data/ospfv2-instance/ospfv2-area/ospfv2-interface/ospfv2-neighbor\n",
               "filter xpath /bgp-ios-xe-oper:bgp-state-data/neighbors/neighbor\n",
-              "filter xpath /bgp-ios-xe-oper:bgp-state-data/address-families/address-family\n",
-              "filter xpath /mdt-oper:mdt-oper-data/mdt-subscriptions/base\n"]
+              "filter xpath /bgp-ios-xe-oper:bgp-state-data/address-families/address-family\n"]
